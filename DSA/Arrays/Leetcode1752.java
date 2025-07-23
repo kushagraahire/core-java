aeb389b8e86489320a7340eec7a925b3f9c654f4
Kushagra Ahire <kushagraahire55@gmail.com>
2025-07-23 21:30:12 +0530

    added leetcode 1752

---
+++ DSA/Arrays/Leetcode1752.java
@@ -0,0 +1,12 @@
+public class Leetcode1752 {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        if(nums[0] < nums[n-1]) count++;
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] > nums[i+1]) count++;
+            if(count > 1) return false;
+        }
+        return true;
+    }
+}
