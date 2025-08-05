0f366acd8205821ad65299808101f2e6dc8cc40e
Kushagra Ahire <kushagraahire55@gmail.com>
2025-08-05 21:49:25 +0530

    Added Leetcode876

---
+++ DSA/LinkedList/Leetcode876.java
@@ -0,0 +1,20 @@
+public class Leetcode876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
