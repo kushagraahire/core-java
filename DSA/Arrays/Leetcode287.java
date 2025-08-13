28fef22019f140682c1cb050403d68f5db151e86
Kushagra Ahire <kushagraahire55@gmail.com>
2025-08-13 21:20:55 +0530

    Added Leetcode287

---
+++ DSA/Arrays/Leetcode287.java
@@ -0,0 +1,27 @@
+public class Leetcode287 {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+        slow = 0;
+
+        while(true){
+            slow = nums[slow];
+            fast = nums[fast];
+
+            if(slow == fast){
+                break;
+            }
+        }
+        return slow;
+    }
+}
