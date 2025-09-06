0b35091d325f62d94253deaf2562c89ec275ed03
Kushagra Ahire <kushagraahire55@gmail.com>
2025-09-06 21:42:08 +0530

    Added Leetcode20.java

---
+++ DSA/Stack/Leetcode20.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Leetcode20 {
+    public boolean isValid(String s) {
+        Stack<Character> leftSymbols = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                leftSymbols.push(c);
+            }
+
+            else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+                leftSymbols.pop();
+            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                leftSymbols.pop();
+            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                leftSymbols.pop();
+            }
+
+            else {
+                return false;
+            }
+        }
+        return leftSymbols.isEmpty();
+    }
+}
