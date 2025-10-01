4f2baabbdf2903440237ed8dc16a80c9a47248ee
Kushagra Ahire <kushagraahire55@gmail.com>
2025-10-01 23:42:55 +0530

    Added Leetcode205

---
+++ DSA/String/Leetcode205.java
@@ -0,0 +1,20 @@
+public class Leetcode205 {
+    public boolean isIsomorphic(String s, String t) {
+        int[] map1 = new int[300];
+        int[] map2 = new int[300];
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(map1[s.charAt(i)] != map2[t.charAt(i)]){
+                return false;
+            }
+
+            map1[s.charAt(i)] = i+1;
+            map2[t.charAt(i)] = i+1;
+        }
+        return true;
+    }
+}
